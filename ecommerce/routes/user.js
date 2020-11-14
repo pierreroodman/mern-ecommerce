214dc31c8a4895d823da5382a488385619ac98a1
pierreroodman <pierreroodman@gmail.com>
2020-11-14 19:17:01 +0200

    added user controller

--- ecommerce/routes/user.js
+++ ecommerce/routes/user.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.send('Hello welcome to my server');
-});
+const {sayHi} = require('../controllers/user')
+
+router.get('/', sayHi);
 
 module.exports = router;
