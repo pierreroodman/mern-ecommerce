9fb88debe06b3424dcf37a63da06b98d214be933
pierreroodman <pierreroodman@gmail.com>
2020-11-14 21:14:21 +0200

    added user signup and password encryption

--- ecommerce/app.js
+++ ecommerce/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose')
 require('dotenv').config()
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 
 
 //import all routes
@@ -15,6 +18,12 @@ mongoose.connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useCreateIndex: true
 }).then(() => console.log('Connected to MongoDB'));
+
+
+//various imported middleware put into use
+app.use(morgan('dev')); //for logging which routes have been used
+app.use(bodyParser.json()); //for parsing through json sent in body of http request
+app.use(cookieParser());
 
 //routes middleware
 app.use("/api", userRoutes);
